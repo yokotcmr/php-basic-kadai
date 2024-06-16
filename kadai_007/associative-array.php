ef719ef13fb7cd807f4126b00dfc9ccbfd26d7bd
yokotcmr <166018980+yokotcmr@users.noreply.github.com>
2024-06-16 22:18:34 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+
+<meta charset="UTF-8">
+<title>課題associative-array.php</title>
+
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetables = ['name' => 'onion', 'price' => '200', 'weight' => '160',];
+
+    print_r($vegetables);
+
+    ?>
+  
+</body>
+</p>
+</html>
+
+
+
